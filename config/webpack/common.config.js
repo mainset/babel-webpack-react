1cde60db86861b32b7743057c06dd9ec23810dab
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2018-10-16 11:49:47 +0300

    [config] webpack add loading of fonts

--- config/webpack/common.config.js
+++ config/webpack/common.config.js
@@ -35,6 +35,17 @@ module.exports = {
             },
           },
         ],
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'dist/assets/fonts',
+            },
+          },
+        ],
       },
     ],
   },
